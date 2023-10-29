590754cafe4eaec80b1dd84b79c5c331272172ec
ShadowLancel <81710119+ShadowLancel@users.noreply.github.com>
2023-10-30 01:12:29 +0300

    Add files via upload

--- PyBotAPI/PyBotAPI.py
+++ PyBotAPI/PyBotAPI.py
@@ -15,14 +15,11 @@ Press Ctrl-C on the command line or send a signal to the process to stop the
 bot.
 """
 from distutils.util import convert_path
-from email.message import Message
-from turtle import update
-import requests
-import json
 import logging
 from telegram.ext import CallbackContext, CallbackQueryHandler
 from telegram import __version__ as TG_VER
 import telegram
+import requests
 
 try:
     from telegram import __version_info__
@@ -39,8 +36,10 @@ from telegram import ForceReply, Update
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters, ConversationHandler
 
 bot = telegram.Bot(token='6263719514:AAGj3bu4SRreXZgEnjal3SgzkO1BJsjRqkE')
-API_TOKEN = '1678302902646058-3164985130563654200-sas3-1045-8f5-sas-l7-balancer-8080-BAL-7874'
-PRODUCT_NAME, REGION_ID, FINAL_ID = range(3)
+url = 'https://svcs.ebay.com/services/search/FindingService/v1'
+operation_name = 'findItemsByKeywords'
+app_id = '-AssisTEM-PRD-8cd4f0a51-f8713116'
+PRODUCT_NAME, PROD_PER_PAGE, FINAL_ID = range(3)
 # Enable logging
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
@@ -60,31 +59,65 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /help is issued."""
-    await update.message.reply_text("BoT gIv 4r U Anketa. Yo fiL 1t sAmm wrds abAut prdUct Nnd h3s taKe f4r u l1nk abut prAdct 0n Russian Tovarisheskiy Magazine YandexMarket.")
+    await update.message.reply_text("BoT gIv 4r U Anketa. Yo fiL 1t sAmm wrds abAut prdUct Nnd h3s taKe f4r u l1nk abut prAdct 0n Magazine eBay.")
 
 async def search(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /search is issued."""
     await update.message.reply_text("4 ur Se4rCh nId Fil ANKETA. FoRsT qWest10n : WhAT N4m3 Pr0dct")
-    return REGION_ID
+    return PROD_PER_PAGE
 
 async def product_name(update, context):
     user_data = {}
     user_data['product_name'] = update.message.text
     context.user_data.update(user_data)
     logger.info('pr_nm: %s', user_data['product_name'])
-    await update.message.reply_text('Please enter your RegionID:')
+    await update.message.reply_text('SeCnd QwsTioN : hAw MatCH Pr0dct U NiD? 1 dO 100')
     return FINAL_ID
 
-async def region_id(update, context):
+async def prod_per_page(update, context):
     user_data = context.user_data
-    user_data['region_id'] = update.message.text
-    # Do something with the user data here
-    logger.info('rg_id: %s', user_data['region_id'])
-    await bot.send_message(chat_id=update.effective_chat.id, text='Thank you for your input!')
+    user_data['prod_per_page'] = update.message.text
+    logger.info('PPP: %s', user_data['prod_per_page'])
+    await bot.send_message(chat_id=update.effective_chat.id, text='ThX U 4r Ur InPt!')
+    product_name = user_data['product_name']
+    prod_per_page = user_data['prod_per_page']
+    # Задаем заголовки запроса
+    headers = {
+    'X-EBAY-SOA-OPERATION-NAME': operation_name,
+    'X-EBAY-SOA-SECURITY-APPNAME': app_id,
+    'X-EBAY-SOA-RESPONSE-DATA-FORMAT': 'JSON'
+              }
+
+    # Параметры запроса
+    params = {
+    'keywords': product_name,
+    'paginationInput': {
+        'entriesPerPage': prod_per_page, # От 1 До 100 запросов
+        'pageNumber': 1
+                       }
+             }
+    # Send a request to the API with the headers and parameters
+    response = requests.get(url, headers=headers, params=params)
+    # Parse the response and send it back to the user via Telegram bot
+    if response.status_code == 200:
+        data = response.json()
+        i = 0
+        search_result = data.get('findItemsByKeywordsResponse')[0].get('searchResult')[0]
+        for item in search_result.get('item'):
+            if i >= int(prod_per_page):
+                break
+            message = f"HIr Ar3 thE reSlts 4 {product_name}: {item.get('title')[0]} . URL: {item.get('viewItemURL')[0]}"
+            await bot.send_message(chat_id=update.effective_chat.id, text=message)
+            i+=1
+            logger.info('i: %s', i)
+    else:
+        message = f"Sry, thEr wAs An Errr wOth th3 API rEqu3st. Errr c0d: {response.status_code}"
+        await bot.send_message(chat_id=update.effective_chat.id, text=message)
+
     return ConversationHandler.END
 
 async def cancel(update, context):
-    await bot.send_message(chat_id=update.effective_chat.id, text='Conversation cancelled.')
+    await bot.send_message(chat_id=update.effective_chat.id, text='C0nvrs4t1On cAncll3d. WrAiT /Start 4 BeG1n sEarCH aGa1n.')
     return ConversationHandler.END
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -99,8 +132,8 @@ def main() -> None:
         entry_points=[CommandHandler('start', start)],
         states={
             PRODUCT_NAME: [CommandHandler('search', search)],
-            REGION_ID: [MessageHandler(filters.TEXT & ~filters.COMMAND, product_name)],
-            FINAL_ID: [MessageHandler(filters.TEXT & ~filters.COMMAND, region_id)]
+            PROD_PER_PAGE: [MessageHandler(filters.TEXT & ~filters.COMMAND, product_name)],
+            FINAL_ID: [MessageHandler(filters.TEXT & ~filters.COMMAND, prod_per_page)]
         },
         fallbacks=[CommandHandler('cancel', cancel)]
     )
